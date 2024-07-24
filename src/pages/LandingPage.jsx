f564efdc1e688425ab9b6a0e6884e4becfdcdecd
Tryb3-Tunchi <bebefinn72@gmail.com>
2024-07-24 15:52:58 -0400

    newsletter

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import NewsLetter from './newsLetter';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <NewsLetter />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
