ab3b27f7acbc2201749a1aab10fb7710b8f16e42
oluseyemichael <oluseyemichael11@gmail.com>
2024-07-25 21:48:48 +0200

    Add Vercel configuration for client-side routing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,11 +3,13 @@ import Footer from '../components/Footer';
 import NewsLetter from '../components/newsLetter';
 import Services from '../components/Services';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
+      <Hero />
       <Services />
       <NewsLetter />
       <Footer />
