bda56fc0f14c29fc2357c94c4c6569f70737926f
oluseyemichael <oluseyemichael11@gmail.com>
2024-07-25 20:28:04 +0200

    More bug fixes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Footer from '../components/Footer';
 import NewsLetter from '../components/newsLetter';
 import Services from '../components/Services';
+import Header from '../components/Header';
 
 const LandingPage = () => {
   return (
     <div>
+      <Header />
       <Services />
       <NewsLetter />
       <Footer />
