854070509674e0ba83d61fbd9aa5606f49aa86f1
Tryb3-Tunchi <bebefinn72@gmail.com>
2024-07-25 13:02:29 -0400

    tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
       colors: {
-        'darkGrey': '#C0BDFF',
+        primary: {
+          blue: '#2452D6',
+          red: '#C50016',
+          green: '#00710B',
+        },
+        secondary: {
+          grey: '#777',
+          grey2: '#F9F8FF',
+        },
+        darkGrey: '#C0BDFF',
         'hover-bg': '#4567F7',
         'text-color': '#14183B',
         'hover-text': '#F9F8FF',
-        'up-arrow' : '#00710B',
-        'add-sub-acc':'#2452D6',
+        'up-arrow': '#00710B',
+        'add-sub-acc': '#2452D6',
         // 'gray': '#E2E2E2'
-        'gray': '#bdbdbd'
-
+        gray: '#bdbdbd',
       },
     },
   },
   plugins: [],
-}
+};
