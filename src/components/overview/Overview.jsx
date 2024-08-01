d8089ec3eef6042d7966a636955b8368376cf2eb
giftedpromise <giftedpromise1992@gmail.com>
2024-08-01 11:40:35 -1200

    udated style

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -7,15 +7,21 @@ import ProgressCard from './ProgressCard';
 
 const Overview = () => {
   return (
-    <div>
-      <div className="flex flex-col lg:flex-row lg:ml-[50px] p-6 space-y-6 lg:space-y-0 lg:space-x-6">
+    <div
+      className="flex-col h-screen w-[100%] overflow-auto"
+      style={{
+        backgroundColor:
+          'var(--Color-Style-Primary-Roles-Pri-container, #E2DDFF)',
+      }}
+    >
+      <div className="flex flex-col lg:flex-row lg:ml-[24px] p-6 space-y-6 lg:space-y-0 lg:space-x-6">
         <ProfileCard />
         <ProgressCard />
       </div>
       <div className="px-6">
         <CardSection />
       </div>
-      <div className="flex flex-col lg:flex-row gap-6 ml-[50px] m-6 pl-6">
+      <div className="flex flex-col lg:flex-row gap-6 ml-[24px] m-6 pl-6">
         <RecentTransactions />
         <ExpenseCard />
       </div>
