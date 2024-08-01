78efd58decd9b971dafc39dade574e02f58d4c12
giftedpromise <giftedpromise1992@gmail.com>
2024-08-01 05:29:26 -1200

    udated style

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,9 @@ import LoginPage from './pages/SIGNIN/LoginPage';
 import SignUpPage from './pages/SIGNUP/SignUpPage';
 import Recovery from './pages/SIGNIN/Recovery';
 
-
 function App() {
   return (
     <Router>
-      
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route
@@ -29,75 +27,52 @@ function App() {
             </MainLayout>
           }
         />
-<<<<<<< HEAD
         <Route
-          path="/overview"
+          path="/sub-account-list"
           element={
-            <div
-              className="flex-col h-screen w-[100%] overflow-auto"
-=======
-
-        <Route path="/sub-account-list" element={
-          <MainLayout showDashboardSidebar={true} showTopbar={true}>
-            <SubAccountList />
-          </MainLayout>
-        }
+            <MainLayout showDashboardSidebar={true} showTopbar={true}>
+              <SubAccountList />
+            </MainLayout>
+          }
         />
-
-        <Route path="/login" element={
-          <LoginPage />
-        }
-        />
-        <Route path="signup" element={<SignUpPage/>} />
-        <Route path="recovery" element={<Recovery/>} />
-        
-        
-
-
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="recovery" element={<Recovery />} />
         <Route
           path="/overview"
           element={
             <MainLayout showDashboardSidebar={true} showTopbar={true}>
-            <div
-              className="flex-col h-screen  overflow-none"
->>>>>>> 9056392d477f4ef3882c08b802c72630a4b30404
-              style={{
-                backgroundColor:
-                  'var(--Color-Style-Primary-Roles-Pri-container, #E2DDFF)',
-              }}
-            >
-              <Overview />
-            </div>
-<<<<<<< HEAD
-          }
-        />
-=======
+              <div
+                className="flex-col h-screen overflow-none"
+                style={{
+                  backgroundColor:
+                    'var(--Color-Style-Primary-Roles-Pri-container, #E2DDFF)',
+                }}
+              >
+                <Overview />
+              </div>
             </MainLayout>
           }
         />
-
-        {/* Add other routes here */}
-
-        <Route path="/transaction-history" element={
-          <MainLayout showDashboardSidebar={true} showTopbar={true}>
-            <Transaction />
-          </MainLayout>
-        }
+        <Route
+          path="/transaction-history"
+          element={
+            <MainLayout showDashboardSidebar={true} showTopbar={true}>
+              <Transaction />
+            </MainLayout>
+          }
         />
-
-        <Route path="/fund-request"
+        <Route
+          path="/fund-request"
           element={
             <MainLayout showDashboardSidebar={true} showTopbar={true}>
               <FundRequest />
             </MainLayout>
           }
         />
-        <Route path="/submit.jsx" element={<Submit />} />
->>>>>>> 9056392d477f4ef3882c08b802c72630a4b30404
-        {/* Add other routes here */}
+        <Route path="/submit" element={<Submit />} />
       </Routes>
     </Router>
-
   );
 }
 
