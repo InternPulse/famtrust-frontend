b9493e0765a8abb2ba429d46617a24429c1d496e
Emeka <emekaamaechi0@gmail.com>
2024-08-07 22:58:10 +0100

    added create sub-account button

--- src/components/SearchAndSort.jsx
+++ src/components/SearchAndSort.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
 const SearchAndSort = () => {
+  // const [searchText, setSearchText] = useState('')
   return (
-    <div className="flex flex-col sm:flex-row justify-between items-center mb-4 overflow-hidden">
+    <div className="flex flex-col sm:flex-row  items-center mb-4 overf">
       {/* Search input and button */}
       <div className="flex w-full sm:w-auto mb-4 sm:mb-0">
         <input
           type="text"
+          onChange={(e) => setSearchText(e.target.value)}
           placeholder="Search..."
-          className="flex-grow p-2 border rounded-l-lg"
+          className="flex-grow p-2 border border-slate-200 rounded-lg"
         />
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-r-lg">
+        <button className="bg-blue-600 text-white mx-4 px-4 py-2 rounded-lg">
           Search
         </button>
       </div>
       {/* Sort dropdown */}
-      <select className="border p-2 rounded">
+      <select className="p-2 rounded-lg bg-darkerGrey text-blue-600">
         <option>Sort by</option>
       </select>
     </div>
