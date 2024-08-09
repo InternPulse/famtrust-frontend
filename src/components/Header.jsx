7fde6769320245a944f7d3564d694dc44e91e110
oluseyemichael <oluseyemichael11@gmail.com>
2024-08-09 12:08:36 +0200

    Fix: FamtrustLogo on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,23 +16,23 @@ const Header = () => {
   }
   return (
     <div className={`py-4 ${clickShow && " overflow-hidden"} `}>
-      <div className=" flex justify-between items-center bg-blueyy md:px-24 px-6">
+      <div className="flex justify-between items-center bg-blueyy md:px-24 px-6">
         <div>
           <NavLink className="flex gap-1 items-center" to="/">
-            <img className="h-fit w-8" src={logo} alt="logo" />
-            <p className="font-montesarrat text-base font-normal text-txtblue">
+            <img className="" src={logo} alt="logo" />
+            {/* <p className="font-montesarrat text-2xl lg:text-4xl font-normal text-txtblue">
               famTrust
-            </p>
+            </p> */}
           </NavLink>
         </div>
         <div onClick={clickShow}>
           {show ? (
             <FaTimes />
           ) : (
-            <GiHamburgerMenu className="text-tx md:hidden" />
+            <GiHamburgerMenu className="text-tx md:hidden text-2xl" />
           )}
         </div>
-        <div className="text-txtblue md:flex gap-5 hidden">
+        <div className="text-txtblue md:flex gap-5 hidden font-semibold">
           <HashLink smooth to="#about">About Us</HashLink>
           <HashLink smooth to="#services">Services</HashLink>
           <HashLink smooth to="#resources">Resources</HashLink>
@@ -40,18 +40,18 @@ const Header = () => {
         </div>
         <div className="md:flex gap-5 hidden">
           <NavLink className="bg-white text-txtblue border-solid border-[1px] border-blue-600  rounded-lg" to="login">
-            <button className="px-5 py-2 ">Log In</button>
+            <button className="px-5 py-2 font-semibold">Log In</button>
           </NavLink>
           <NavLink
             className="bg-bluey px-5 py-2 rounded-lg text-white"
             to="signup"
           >
-            <button>Sign Up</button>
+            <button className="font-semibold">Sign Up</button>
           </NavLink>
         </div>
       </div>
-      <div className={`md:hidden flex flex-col text-center px-6 h-screen mt-4 transition-all py-4 bg-black text-white ${show ? "block" : "hidden"}`}>
-        <div onClick={closeShow} className="flex flex-col py-6 ">
+      <div className={`md:hidden flex flex-col text-center px-6 h-screen mt-4 transition-all py-4 bg-black text-white ${show ? "block" : "hidden"} font-semibold`}>
+        <div onClick={closeShow} className="flex flex-col py-6">
           <HashLink smooth className="py-4" to="#about">About Us</HashLink>
           <HashLink smooth className="py-4" to="#services">Services</HashLink>
           <HashLink smooth className="py-4" to="#resources">Resources</HashLink>
@@ -59,10 +59,10 @@ const Header = () => {
         </div>
         <div onClick={closeShow} className="flex flex-col py-4 ">
           <NavLink className="bg-white text-txtblue my-6 rounded-lg" to="login">
-            <button className="px-3 py-4 ">Login</button>
+            <button className="px-3 py-4 font-semibold">Login</button>
           </NavLink>
           <NavLink className="bg-bluey px-3 py-4 rounded-lg text-white" to="signup">
-            <button>Sign Up</button>
+            <button className="font-semibold">Sign Up</button>
           </NavLink>
         </div>
       </div>
